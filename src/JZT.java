31d524cad34b4bf74a757d3f245748740b17eb1b
PGZ <2234035352@qq.com>
2024-07-15 18:20:13 +0800

    added JZT.java

---
+++ src/JZT.java
@@ -0,0 +1,21 @@
+public class JZT {
+    public static void main(String[] args) {
+
+        int row = 0;
+        int size = 5;
+        while (row < size){
+            row = row + 1;
+            int col = 0 ;
+            while (col<row){
+
+                System.out.print("*");
+                col++;}
+            System.out.println();
+
+
+        }
+    }
+}
+
+
+
